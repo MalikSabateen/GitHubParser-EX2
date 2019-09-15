5d446c8bf116274425ac7373f4e8aa37f22c9c4a
Malik Sabateen <31102155+MalikSabateen@users.noreply.github.com>
2019-09-15 14:39:30 +0930

    Everything except comments & whitespace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ## Imports
 import git
 from git import Repo
+from datetime import datetime
+
 choice=input("If you have the GitHub Repo already downloaded please enter 'yes', if you would like to download it please enter 'no'.\n")
 if choice == "yes":
     repo_link=input("Please enter the location of the GitHub Repo you would like to analyse.\n")
@@ -17,14 +19,38 @@ fix_commit=repo.commit(commit_id)
 print(fix_commit.message)
 stats=fix_commit.stats
 print("---Number of Files Changed---")
-print(len(stats.files))
+print("\t",len(stats.files))
 list=[]
 for item in stats.files:
     if(item.rsplit("/",1)[0]) not in list:
         list.append(item.rsplit("/",1)[0])
 print("---Number of Directories Changed---")
-print(len(list))
+print("\t",len(list))
 print("---Number of Lines Deleted (With Comments & Blank Lines)---")
-print(stats.total['deletions'])
+print("\t",stats.total['deletions'])
 print("---Number of Lines Inserted (With Comments & Blank Lines)---")
-print(stats.total['insertions'])
+print("\t",stats.total['insertions'])
+print("---Time between the Fixing Commit and the Previous Commit---")
+dateone=datetime.strptime(repo.git.log(fix_commit,n=1,format="%cd"),'%a %b %d %H:%M:%S %Y %z')
+datetwo=datetime.strptime(repo.git.log(fix_commit.parents[0],n=1,format="%cd"),'%a %b %d %H:%M:%S %Y %z')
+print("\t",dateone-datetwo)
+print("---Number of Modifications for each File---")
+for item in stats.files:
+    print("\t--"+item+"--")
+    list=repo.git.log(item,follow=True)
+    print("\t\t",len(list))
+print("---Developers which have made Modifications to a File---")
+for item in stats.files:
+    print("\t--"+item+"--")
+    list=[]
+    listnum=[]
+    authors=repo.git.log(item,format="%an").split("\n")
+    for author in authors:
+        if (author.casefold() not in list):
+            list.append(author.casefold())
+            listnum.append(author.casefold())
+        elif (author.casefold() in list):
+            listnum.append(author.casefold())
+    for name in list:
+        print("\t\tAuthor: ",name.casefold())
+        print("\t\t\tNumber of Changes: ",listnum.count(name.casefold()))
